f38fb9965b5aa9400640f1744efa4151302a679e
Shu-Sam <shu5388369@gmail.com>
2022-04-05 16:40:56 +0300

    rubocop edits

--- app/controllers/car_parks_controller.rb
+++ app/controllers/car_parks_controller.rb
@@ -2,7 +2,6 @@ class CarParksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   before_action :set_car_park, only: %i[show edit update destroy]
 
-
   def index
     if authenticate_user! && current_user.owner_role?
       @car_parks = current_user.owned_car_parks
@@ -13,59 +12,44 @@ class CarParksController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @car_park = CarPark.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @car_park = CarPark.new(car_park_params)
     @car_park.owner = current_user
-
-    respond_to do |format|
-      if @car_park.save
-        format.html { redirect_to car_park_url(@car_park), notice: "Car park was successfully created." }
-        format.json { render :show, status: :created, location: @car_park }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @car_park.errors, status: :unprocessable_entity }
-      end
+    if @car_park.save
+      redirect_to car_park_url(@car_park), notice: 'Car park was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @car_park.update(car_park_params)
-        format.html { redirect_to car_park_url(@car_park), notice: "Car park was successfully updated." }
-        format.json { render :show, status: :ok, location: @car_park }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @car_park.errors, status: :unprocessable_entity }
-      end
+    if @car_park.update(car_park_params)
+      redirect_to car_park_url(@car_park), notice: 'Car park was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @car_park.destroy
-
-    respond_to do |format|
-      format.html { redirect_to car_parks_url, notice: "Car park was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to car_parks_url, notice: 'Car park was successfully destroyed.'
   end
 
   private
 
-    def set_car_park
-      @car_park = CarPark.find(params[:id])
-    end
+  def set_car_park
+    @car_park = CarPark.find(params[:id])
+  end
 
-    def car_park_params
-      params.require(:car_park).permit(:title, :address, :parking_type, :usage_fee, :discount, :spaces)
-    end
+  def car_park_params
+    params.require(:car_park).permit(:title, :address, :parking_type, :usage_fee, :discount, :spaces)
+  end
 end
